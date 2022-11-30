a65004ec9a4787d185008220ba0ee0b41aefef7e
shiva <tshi22152.cs@rmkec.ac.in>
2022-11-30 10:44:01 +0530

    i changed earth to world

---
+++ SDP/SDPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDPApp.swift
+//  SDP
+//
+//  Created by shiva shangaran on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SDPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
